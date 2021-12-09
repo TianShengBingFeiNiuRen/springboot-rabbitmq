22f9bfc2e201bf05b14eed458ac1856c037b43ca
Andon <17610835229@163.com>
2021-12-09 15:38:35 +0800

    SpringBoot 整合 RabbitMQ

--- src/main/java/com/andon/springbootrabbitmq/rabbitmq/RabbitMQConsumer.java
+++ src/main/java/com/andon/springbootrabbitmq/rabbitmq/RabbitMQConsumer.java
@@ -13,52 +13,52 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class RabbitMQConsumer {
-//
-//    // work1
-//    @RabbitListener(queues = "${rabbitmq.work.queue-name}")
-//    public void consumeWork1(String message) {
-//        log.info("consumeWork1 message:{}", message);
-//    }
-//
-//    // work2
-//    @RabbitListener(queues = "${rabbitmq.work.queue-name}")
-//    public void consumeWork2(String message) {
-//        log.info("consumeWork2 message:{}", message);
-//    }
-//
-//    // 订阅1
-//    @RabbitListener(queues = "${rabbitmq.fanout.queue-name-1}")
-//    public void consumeFanout1(String message) {
-//        log.info("consumeFanout1 message:{}", message);
-//    }
-//
-//    // 订阅2
-//    @RabbitListener(queues = "${rabbitmq.fanout.queue-name-2}")
-//    public void consumeFanout2(String message) {
-//        log.info("consumeFanout2 message:{}", message);
-//    }
-//
-//    // topic1
-//    @RabbitListener(queues = "${rabbitmq.topic.queue-name-1}")
-//    public void consumeTopic1(String message) {
-//        log.info("consumeTopic1 message:{}", message);
-//    }
-//
-//    // topic2
-//    @RabbitListener(queues = "${rabbitmq.topic.queue-name-2}")
-//    public void consumeTopic2(String message) {
-//        log.info("consumeTopic2 message:{}", message);
-//    }
-//
-//    // 消息confirm机制
-//    @RabbitListener(queues = "${rabbitmq.confirm.queue-name}")
-//    public void consumeConfirm(String message) {
-//        log.info("consumeConfirm message:{}", message);
-//    }
-//
-//    // 消息return机制
-//    @RabbitListener(queues = "${rabbitmq.return.queue-name}")
-//    public void consumeReturn(String message) {
-//        log.info("consumeReturn message:{}", message);
-//    }
+
+    // work1
+    @RabbitListener(queues = "${rabbitmq.work.queue-name}")
+    public void consumeWork1(String message) {
+        log.info("consumeWork1 message:{}", message);
+    }
+
+    // work2
+    @RabbitListener(queues = "${rabbitmq.work.queue-name}")
+    public void consumeWork2(String message) {
+        log.info("consumeWork2 message:{}", message);
+    }
+
+    // 订阅1
+    @RabbitListener(queues = "${rabbitmq.fanout.queue-name-1}")
+    public void consumeFanout1(String message) {
+        log.info("consumeFanout1 message:{}", message);
+    }
+
+    // 订阅2
+    @RabbitListener(queues = "${rabbitmq.fanout.queue-name-2}")
+    public void consumeFanout2(String message) {
+        log.info("consumeFanout2 message:{}", message);
+    }
+
+    // topic1
+    @RabbitListener(queues = "${rabbitmq.topic.queue-name-1}")
+    public void consumeTopic1(String message) {
+        log.info("consumeTopic1 message:{}", message);
+    }
+
+    // topic2
+    @RabbitListener(queues = "${rabbitmq.topic.queue-name-2}")
+    public void consumeTopic2(String message) {
+        log.info("consumeTopic2 message:{}", message);
+    }
+
+    // 消息confirm机制
+    @RabbitListener(queues = "${rabbitmq.confirm.queue-name}")
+    public void consumeConfirm(String message) {
+        log.info("consumeConfirm message:{}", message);
+    }
+
+    // 消息return机制
+    @RabbitListener(queues = "${rabbitmq.return.queue-name}")
+    public void consumeReturn(String message) {
+        log.info("consumeReturn message:{}", message);
+    }
 }
